d76cb19c1b11797cef38cefa62d6c3340200a46e
StasyHard <reyngardt.as@yandex.ru>
2020-04-01 18:41:52 +0300

    Initial

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Anastasia Reyngardt on 01.04.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
