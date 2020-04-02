a18ad012bcccaba571a65bb8336d236cbe03aaaa
StasyHard <reyngardt.as@yandex.ru>
2020-04-02 12:07:19 +0300

    Add EmojiArtView class

---
+++ EmojiArt/EmojiArtView.swift
@@ -0,0 +1,14 @@
+//
+//  EmogiArtView.swift
+//  EmojiArt
+//
+//  Created by Anastasia Reyngardt on 02.04.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtView: UIView {
+
+
+}
