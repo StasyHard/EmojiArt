ef509bf7ad2487e5da3dea30a1e8309701c53dd2
StasyHard <reyngardt.as@yandex.ru>
2020-04-02 15:28:16 +0300

    Add SplitVC and EmojiArtDocumentTableVC with data source methods

---
+++ EmojiArt/EmojiArtDocumentTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  EmojiArtDocumentTableViewController.swift
+//  EmojiArt
+//
+//  Created by Anastasia Reyngardt on 02.04.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtDocumentTableViewController: UITableViewController {
+    
+    var emojiArtDocuments = ["One", "Two", "Tree"]
+
+    @IBAction func newEmojiArt(_ sender: Any) {
+        emojiArtDocuments += ["Untitled".madeUnique(withRespectTo: emojiArtDocuments)]
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiArtDocuments.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
+        cell.textLabel?.text = emojiArtDocuments[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            emojiArtDocuments.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            
+        }
+    }
+
+}
