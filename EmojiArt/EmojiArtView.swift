f143d4ecb8835b8230bd1df8c36ff1af66e8eb08
StasyHard <reyngardt.as@yandex.ru>
2020-04-02 12:11:31 +0300

    Add backgroundImage draw

--- EmojiArt/EmojiArtView.swift
+++ EmojiArt/EmojiArtView.swift
@@ -10,5 +10,10 @@ import UIKit
 
 class EmojiArtView: UIView {
 
+    var backgroundImage: UIImage? { didSet { setNeedsDisplay() } }
+    
+    override func draw(_ rect: CGRect) {
+        backgroundImage?.draw(in: rect)
+    }
 
 }
